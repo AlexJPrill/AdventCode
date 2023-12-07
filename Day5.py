7b25ddadf989e790f11c805b3c3dd613702710a2
AlexJPrill <alex.j.prill@gmail.com>
2023-12-07 12:58:44 -0600

    update

--- Day5.py
+++ Day5.py
@@ -1,11 +1,27 @@
 
+class Soil:
+
+    def __init__(self):
+        self.source = -1
+        self.destination = -1
+        self.range = -1
+
+
+class Map:
+
+    def __init__(self, name):
+        self.name = name
+        self.soils = []
 
 
 seedsToPlant = []
+maps = []
 
 file = open("input.txt", "r")
 l = file.readline()
 l = l.split(":")[1].strip()
 seedsToPlant = l.split(" ")
-print(seedsToPlant)
+
+
+        
 #dog
